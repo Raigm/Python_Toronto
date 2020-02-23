23b937e73f2ccacb3b37656414c25eb2c2b4a1d3
Raigm <rai.gordejuela@gmail.com>
2020-02-23 19:13:33 +0100

    function word_score

--- a3/test_week6.py
+++ a3/test_week6.py
@@ -2,7 +2,7 @@
 from a3 import *
 
 list = [['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']]
-word = "AX2"
+word = "DRUDGERY"
 
 # #testing functions
 # print(is_valid_word(["ALSE", "TU"], "TU"))
@@ -23,3 +23,26 @@ print(board_contains_word_in_column(list, "AX"))
 
 print(board_contains_word([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 'ANTa'))
 # "True"
+
+
+print(word_score(word))
+"1"
+
+
+if len(word) >= 10:
+    print (len(word)* 3)
+elif len(word) >= 6 and len(word) < 9 :
+    print(len (word) * 2)
+elif len(word) < 6 and len(word) >= 3:
+    print(len(word) * 1 )
+else:
+    print(0)
+
+    # Word length: < 3: 0 points
+    #              3-6: 1 point per character for all characters in word
+    #              7-9: 2 points per character for all characters in word
+    #              10+: 3 points per character for all characters in word
+    #
+    # >>> word_score('DRUDGERY')
+    # 16
+    # """
